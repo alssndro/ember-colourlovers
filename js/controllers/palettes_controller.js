bf1ea960a89eb0fd1da7e012c196fbe0701c26db
Alessandro <hello@alessndro.co.uk>
2014-09-06 23:42:35 +0100

    Added basic page navigation UI.

--- js/controllers/palettes_controller.js
+++ js/controllers/palettes_controller.js
@@ -3,8 +3,17 @@ Colourlovers.PalettesController = Ember.ArrayController.extend({
   listType: "top",
   queryParams: ['pageNo', 'listType'],
   itemController: "palette",
-  
+
+  previousPage: function() {
+    var previousPageNo = this.get("pageNo") - 1;
+    return previousPageNo < 1 ? 1 : previousPageNo;
+  }.property("pageNo"),
+
   nextPage: function() {
-    return this.get("pageNo") + 1
+    return this.get("pageNo") + 1;
+  }.property("pageNo"),
+
+  firstPage: function() {
+    return this.get("pageNo") === 1;
   }.property("pageNo")
 });
